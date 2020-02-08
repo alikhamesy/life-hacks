b718a8eadc16d07759800e40ef0ec9d8528513ad
JoBapla <jovanbapla@gmail.com>
2020-02-08 15:58:48 -0800

    jovans shits

--- life-hacks-client/src/components/TopNav.js
+++ life-hacks-client/src/components/TopNav.js
@@ -5,7 +5,7 @@ function TopNav(props) {
     const {onclickhandler} = props
     return (
         <div id="top-nav">
-            <button id="Random" value='random' onClick={onclickhandler}>Random</button>
+            <button id="Random" value='random' onClick={onclickhandler}>Shuffle</button>
             <button id="Custom" value='custom' onClick={onclickhandler}>Custom</button>
          
         </div>
