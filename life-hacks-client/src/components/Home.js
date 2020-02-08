f31f3ef8f14058752f1db6be10a120571279bd35
JoBapla <jovanbapla@gmail.com>
2020-02-08 14:53:28 -0800

    home page

--- life-hacks-client/src/components/Home.js
+++ life-hacks-client/src/components/Home.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import TopNav from './TopNav';
+import './Home.css'
 
 function Home(props) {
     return (
         <div>
-             <TopNav onclickhandler={props.onclickhandler}/>
-            <p>Hello World</p>
-            <p>Goodbye World!</p>
+             <div id= "h">
+            <h1>Foodenator</h1>
+            </div>
+            <TopNav onclickhandler={props.onclickhandler}/>
+        
         </div>
     )
 }
--- life-hacks-client/src/components/TopNav.js
+++ life-hacks-client/src/components/TopNav.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import './Home.css'
 
 function TopNav(props) {
     const {onclickhandler} = props
